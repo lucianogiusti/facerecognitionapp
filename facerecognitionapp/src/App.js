2b3db8109bcd3929031f3c762d0e80626507bafd
Luciano Giusti <lucianogiusti@outlook.com.ar>
2020-04-16 21:08:43 -0300

    connecting to backend

--- facerecognitionapp/src/App.js
+++ facerecognitionapp/src/App.js
@@ -31,7 +31,7 @@ const particlesOptions = {
 class App extends Component {
   constructor() {
     super();
-    this.state = {
+      this.state = {
       input: '',
       imageUrl: '',
       box: {},

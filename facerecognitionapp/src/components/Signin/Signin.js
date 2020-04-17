a5d933cd6783fcac26c931dd008305f62788ac3b
Luciano Giusti <lucianogiusti@outlook.com.ar>
2020-04-17 19:26:53 -0300

    finishing register section

--- facerecognitionapp/src/components/Signin/Signin.js
+++ facerecognitionapp/src/components/Signin/Signin.js
@@ -25,8 +25,9 @@ class Signin extends React.Component {
             })
         })
             .then(response => response.json())
-            .then(data => {
-                if (data === 'success') {
+            .then(user => {
+                if (user.id) {
+                    this.props.loadUser(user);
                     this.props.onRouteChange('home');
                 }
                 
@@ -35,7 +36,7 @@ class Signin extends React.Component {
 
     render() {
         const { onRouteChange } = this.props;
-   return (
+    return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
         <div className="measure">

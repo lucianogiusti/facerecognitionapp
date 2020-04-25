37eba831b920664fa263725060d1b820f4d5be15
Luciano Giusti <lucianogiusti@outlook.com.ar>
2020-04-24 21:38:37 -0300

    security upgrades

--- facerecognitionapp/src/App.js
+++ facerecognitionapp/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
@@ -9,12 +8,6 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
-
-
-const app = new Clarifai.App({
-  apiKey: '4af1c10fd8c04da18aca0a13a7675bd2'
-})
-
 
 const particlesOptions = {
   particles: {
@@ -27,12 +20,8 @@ const particlesOptions = {
     }
   }
 }
-
-class App extends Component {
-  constructor() {
-    super();
-      this.state = {
-      input: '',
+const initialState = {
+    input: '',
       imageUrl: '',
       box: {},
       route: 'signin',
@@ -45,6 +34,11 @@ class App extends Component {
         joined: ''
       }
     }
+
+class App extends Component {
+  constructor() {
+    super();
+      this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -80,10 +74,15 @@ onInputChange = (event) => {
 }
 
 onButtonSubmit = () => {
-  this.setState({imageUrl: this.state.input})
-  app.models.predict(
-    Clarifai.FACE_DETECT_MODEL,
-    this.state.input)
+  this.setState({imageUrl: this.state.input});
+    fetch('http://localhost:3000/imageUrl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+      input: this.state.input
+      })
+    })
+    .then(response => response.json())
     .then(response => { 
       if (response) {
         fetch('http://localhost:3000/image', {
@@ -97,8 +96,8 @@ onButtonSubmit = () => {
         .then(count => {
           this.setState(Object.assign(this.state.user, { entries: count }))
           })
-        
-      }
+          .catch(console.log)
+        }
       this.displayFaceBox(this.calculateFaceLocation(response))
     })
     .catch(err => console.log(err));
@@ -106,7 +105,7 @@ onButtonSubmit = () => {
 
 onRouteChange = (route) => {
   if (route === 'signout') {
-    this.setState({isSignedIn: false})
+    this.setState(initialState)
   } else if (route === 'home') {
     this.setState({isSignedIn: true})
   }

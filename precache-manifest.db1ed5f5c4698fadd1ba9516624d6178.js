e5912408895e3d1846dbb6714d7db7822fa7b4a9
Luciano Giusti <lucianogiusti@outlook.com.ar>
2020-04-25 19:59:05 -0300

    Updates

---
+++ precache-manifest.db1ed5f5c4698fadd1ba9516624d6178.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ebe274d87044efd9ba12e70c5be89d2",
+    "url": "/facerecognitionapp/index.html"
+  },
+  {
+    "revision": "6ec7360333b37b94ed0d",
+    "url": "/facerecognitionapp/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "5f9b7ded339e03471f33",
+    "url": "/facerecognitionapp/static/css/main.27485e67.chunk.css"
+  },
+  {
+    "revision": "6ec7360333b37b94ed0d",
+    "url": "/facerecognitionapp/static/js/2.c1e03b05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognitionapp/static/js/2.c1e03b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f9b7ded339e03471f33",
+    "url": "/facerecognitionapp/static/js/main.af20d47d.chunk.js"
+  },
+  {
+    "revision": "95f5fd9377923e62617c",
+    "url": "/facerecognitionapp/static/js/runtime-main.2ec4e802.js"
+  }
+]);

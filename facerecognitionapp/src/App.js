76decb8062f6b97f8d446e1c79525aad1eed00c1
Luciano Giusti <lucianogiusti@outlook.com.ar>
2020-04-25 20:04:37 -0300

    title

--- facerecognitionapp/src/App.js
+++ facerecognitionapp/src/App.js
@@ -9,6 +9,10 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+componentDidMount() {
+  document.title = 'FaceRecognitionApp by PuchoG';
+}
+
 const particlesOptions = {
   particles: {
     number: {
